13bdb3a093c2de7aab0ff87130fb768826452d41
bala-amypo <bala@amypo.in>
2025-12-20 17:51:25 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
@@ -0,0 +1,18 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.dto.*;
+import com.example.demo.security.JwtTokenProvider;
+import com.example.demo.service.AuthService;
+
+public class AuthServiceImpl implements AuthService {
+
+    private final JwtTokenProvider tokenProvider = new JwtTokenProvider();
+
+    @Override
+    public AuthResponse login(AuthRequest request) {
+        String token = tokenProvider.generateToken(
+                request.getEmail(), "USER", 1L
+        );
+        return new AuthResponse(token);
+    }
+}
