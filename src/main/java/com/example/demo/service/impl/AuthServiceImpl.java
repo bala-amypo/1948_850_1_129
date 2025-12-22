9bcf035672c3ad4481ead63a1f55699a8b00181d
bala-amypo <bala@amypo.in>
2025-12-22 00:46:59 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
@@ -1,31 +1,23 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.model.User;
-import com.example.demo.repository.UserRepository;
-import com.example.demo.service.AuthService;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.dto.AuthRequest;
+import com.example.demo.dto.AuthResponse;
+import com.example.demo.service.AuthService;
 
 @Service
 public class AuthServiceImpl implements AuthService {
 
-    @Autowired
-    private UserRepository userRepository;
-
     @Override
-    public String login(String username, String password) {
+    public AuthResponse login(AuthRequest request) {
 
-        User user = userRepository.findByUsername(username).orElse(null);
+        if ("admin".equals(request.getUsername())
+                && "admin123".equals(request.getPassword())) {
 
-        if (user == null) {
-            return "Invalid credentials";
+            return new AuthResponse("Login successful");
         }
 
-        if (!password.equals(user.getPassword())) {
-            return "Invalid credentials";
-        }
-
-        return "Login successful";
+        return new AuthResponse("Invalid credentials");
     }
 }
