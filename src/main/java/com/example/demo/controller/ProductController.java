e4d49e51bca286a2900eef803d0ea27e0c07ecd6
bala-amypo <bala@amypo.in>
2025-12-25 16:09:20 +0000

    User commit

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -1,6 +1,7 @@
+package com.example.demo.controller;
+
 import com.example.demo.model.Product;
 import com.example.demo.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,17 +10,20 @@ import java.util.List;
 @RequestMapping("/products")
 public class ProductController {
 
-    @Autowired
-    private ProductService service;
+    private final ProductService service;
+
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
 
     @PostMapping
     public Product create(@RequestBody Product product) {
         return service.createProduct(product);
     }
 
-    @PutMapping("/{id}")
-    public Product update(@PathVariable Long id, @RequestBody Product product) {
-        return service.updateProduct(id, product);
+    @GetMapping
+    public List<Product> getAll() {
+        return service.getAllProducts();
     }
 
     @GetMapping("/{id}")
@@ -27,9 +31,9 @@ public class ProductController {
         return service.getProductById(id);
     }
 
-    @GetMapping
-    public List<Product> getAllProducts() {
-        return service.getAllProducts();
+    @PutMapping("/{id}")
+    public Product update(@PathVariable Long id, @RequestBody Product product) {
+        return service.updateProduct(id, product);
     }
 
     @DeleteMapping("/{id}")
