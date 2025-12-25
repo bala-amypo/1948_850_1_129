e8d789de9f538472824d37f152682fee17f23421
bala-amypo <bala@amypo.in>
2025-12-25 13:56:19 +0000

    User commit

--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -1,29 +1,27 @@
 package com.example.demo.model;
 
-import jakarta.persistence.*;
 import java.math.BigDecimal;
 
-@Entity
 public class Product {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(unique = true)
     private String sku;
-
     private String name;
     private BigDecimal price;
     private Boolean active = true;
 
     public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
     public String getSku() { return sku; }
     public void setSku(String sku) { this.sku = sku; }
+
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
+
     public BigDecimal getPrice() { return price; }
     public void setPrice(BigDecimal price) { this.price = price; }
+
     public Boolean getActive() { return active; }
     public void setActive(Boolean active) { this.active = active; }
 }
