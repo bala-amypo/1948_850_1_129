dac7f4d1e2a5e7fb573944a3030f6f365b047675
bala-amypo <bala@amypo.in>
2025-12-25 16:32:51 +0000

    User commit

--- src/main/java/com/example/demo/service/CartService.java
+++ src/main/java/com/example/demo/service/CartService.java
@@ -3,8 +3,6 @@ package com.example.demo.service;
 import com.example.demo.model.Cart;
 
 public interface CartService {
-
     Cart createCart(Long userId);
-
     Cart getActiveCartForUser(Long userId);
 }
