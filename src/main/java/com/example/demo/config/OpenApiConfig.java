d97c429d48f78f155d6d7ec7637ab380189e3897
bala-amypo <bala@amypo.in>
2025-12-25 15:19:19 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,20 +1,31 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
+    // Basic OpenAPI info
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                // You need to change the port as per your server
-                .servers(List.of(
-                        new Server().url("https://9249.408procr.amypo.ai/")
-                ));
-        }
+                .info(new Info()
+                        .title("E-Commerce Bundle Save API")
+                        .version("1.0")
+                        .description("API documentation for Bundle Save project"));
+    }
+
+    // Scan all controllers in base package
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("ecommerce")
+                .packagesToScan("com.example.demo.controller")
+                .pathsToMatch("/**")
+                .build();
+    }
 }
