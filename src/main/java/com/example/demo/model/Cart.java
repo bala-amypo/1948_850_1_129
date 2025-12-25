343036ef3be599e3d566fa1c2c38476b2e249775
bala-amypo <bala@amypo.in>
2025-12-25 13:59:19 +0000

    User commit

--- src/main/java/com/example/demo/model/Cart.java
+++ src/main/java/com/example/demo/model/Cart.java
@@ -1,20 +1,17 @@
 package com.example.demo.model;
 
-import jakarta.persistence.*;
-
-@Entity
 public class Cart {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private Long userId;
     private Boolean active = true;
 
     public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
     public Long getUserId() { return userId; }
     public void setUserId(Long userId) { this.userId = userId; }
+
     public Boolean getActive() { return active; }
     public void setActive(Boolean active) { this.active = active; }
 }
