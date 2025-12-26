83dc673941797201566e510a2895800d6b54ba24
bala-amypo <bala@amypo.in>
2025-12-26 14:20:08 +0000

    User commit

--- src/main/java/com/example/demo/model/BundleRule.java
+++ src/main/java/com/example/demo/model/BundleRule.java
@@ -13,43 +13,27 @@ public class BundleRule {
 
     private String requiredProductIds;
 
-    private Integer discountPercentage;
+    private Double discountPercentage;
 
-    private Boolean active;
+    private Boolean active = true;
 
-    public Long getId() {
-        return id;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    public String getRuleName() {
-        return ruleName;
-    }
+    public String getRuleName() { return ruleName; }
+    public void setRuleName(String ruleName) { this.ruleName = ruleName; }
 
-    public void setRuleName(String ruleName) {
-        this.ruleName = ruleName;
-    }
-
-    public String getRequiredProductIds() {
-        return requiredProductIds;
-    }
-
+    public String getRequiredProductIds() { return requiredProductIds; }
     public void setRequiredProductIds(String requiredProductIds) {
         this.requiredProductIds = requiredProductIds;
     }
 
-    public Integer getDiscountPercentage() {
-        return discountPercentage;
-    }
-
-    public void setDiscountPercentage(Integer discountPercentage) {
+    public Double getDiscountPercentage() { return discountPercentage; }
+    public void setDiscountPercentage(Double discountPercentage) {
         this.discountPercentage = discountPercentage;
     }
 
-    public Boolean getActive() {
-        return active;
-    }
-
-    public void setActive(Boolean active) {
-        this.active = active;
-    }
+    public Boolean getActive() { return active; }
+    public void setActive(Boolean active) { this.active = active; }
 }
+
