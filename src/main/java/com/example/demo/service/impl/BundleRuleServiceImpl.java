e98dc0883e37f06dee01b2595a73e8e25b22aebe
bala-amypo <bala@amypo.in>
2025-12-26 00:51:37 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/BundleRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/BundleRuleServiceImpl.java
@@ -2,10 +2,11 @@ package com.example.demo.service.impl;
 
 import com.example.demo.model.BundleRule;
 import com.example.demo.repository.BundleRuleRepository;
+import com.example.demo.service.BundleRuleService;
 import org.springframework.stereotype.Service;
 
-@Service
-public class BundleRuleServiceImpl {
+@Service   // 🔴 THIS IS MANDATORY
+public class BundleRuleServiceImpl implements BundleRuleService {
 
     private final BundleRuleRepository bundleRuleRepository;
 
@@ -13,18 +14,8 @@ public class BundleRuleServiceImpl {
         this.bundleRuleRepository = bundleRuleRepository;
     }
 
+    @Override
     public BundleRule createRule(BundleRule rule) {
-
-        if (rule.getRequiredProductIds() == null || rule.getRequiredProductIds().trim().isEmpty()) {
-            throw new IllegalArgumentException("Required products cannot be empty");
-        }
-
-        if (rule.getDiscountPercentage() == null ||
-                rule.getDiscountPercentage() < 0 ||
-                rule.getDiscountPercentage() > 100) {
-            throw new IllegalArgumentException("Discount percentage must be between 0 and 100");
-        }
-
         return bundleRuleRepository.save(rule);
     }
 }
